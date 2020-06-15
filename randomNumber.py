db5b9ae00363c3ea3cbc9346a215f4242e3b0d95
daniel <daniel.velden@med.uni-goettingen.de>
2020-06-15 15:43:26 +0000

    rolls_2_adjust

--- randomNumber.py
+++ randomNumber.py
@@ -44,15 +44,15 @@ def get_color_by_dice_naive(spots):
 if __name__ == "__main__":
     outputfilename = "randomNumber"
     print("debug print")
-    rolls = []
+    rolls_2 = []
     color = []
     for i in range (6):
         roll = get_random_number(1, 6)
         color = get_color_by_dice_roll(roll)
         write_log_file(outputfilename, color)
-        rolls.append(roll)
-    print(rolls)
-    plt.bar(range(6), rolls)
+        rolls_2.append(roll)
+    print(rolls_2)
+    plt.bar(range(6), rolls_2)
     plt.show()
     sys.stdout.flush()
     
